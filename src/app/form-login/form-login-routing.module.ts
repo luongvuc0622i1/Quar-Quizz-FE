e90a321edd508c50441516e8d50e56d313551026
Compass <ndphong165@gmail.com>
2022-12-07 21:47:30 +0700

    done form login - register, not biding data

--- src/app/form-login/form-login-routing.module.ts
+++ src/app/form-login/form-login-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {RegisterComponent} from "./register/register.component";
+import {UserComponent} from "../user/user.component";
 
 export const FormLoginRouting: Routes = [
+  { path: 'profile',       component: UserComponent},
   { path: 'register',      component: RegisterComponent },
+
 ]
 
 @NgModule({
--- src/app/form-login/register/register.component.ts
+++ src/app/form-login/register/register.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  ngAfterViewInit() {
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('container');
+
+    signUpButton.addEventListener('click', () => {
+      container.classList.add("right-panel-active");
+    });
+
+    signInButton.addEventListener('click', () => {
+      container.classList.remove("right-panel-active");
+    });
+  }
+
 
 }
