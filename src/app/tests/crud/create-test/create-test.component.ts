88c66629c6d3d155d1838751bd685c53ab6425bb
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2022-12-08 13:02:50 +0700

    done test FE menthod create

--- src/app/tests/crud/create-test/create-test.component.ts
+++ src/app/tests/crud/create-test/create-test.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {TestService} from "../../service/test.service";
 import {FormControl, FormGroup} from "@angular/forms";
+import {Test} from "../../model/test";
+import {Level} from "../../model/level";
+import {Quiz} from "../../model/quiz";
+import {Category} from "../../model/category";
 
 @Component({
   selector: 'app-create-test',
@@ -8,15 +12,20 @@ import {FormControl, FormGroup} from "@angular/forms";
   styleUrls: ['./create-test.component.scss']
 })
 export class CreateTestComponent implements OnInit {
-  constructor(private testService: TestService) {
-  }
+  levels: Level[] = [];
+  quizzes: Quiz[] = [];
+  categories: Category[] = [];
+
+  constructor(private testService: TestService) { }
 
   ngOnInit(): void {
+    this.getAll();
   }
 
   testForm: FormGroup = new FormGroup({
     name: new FormControl(),
     level: new FormControl(),
+    quiz: new FormControl(),
     category: new FormControl(),
     passScore: new FormControl(),
     maxTime: new FormControl(),
@@ -24,11 +33,26 @@ export class CreateTestComponent implements OnInit {
 
   submit(){
     const test= this.testForm.value;
-    this.testService.save(test).subscribe(() =>{
+    console.log(test);
+    const test1 = this.testService.change(test);
+    console.log(test1);
+    this.testService.save(test1).subscribe(() =>{
       this.testForm.reset();
-      alert('Tạo mới thành công');
+      alert('Create done!');
     }, error => {
       console.log(error)
+    });
+  }
+
+  getAll() {
+    this.testService.getLevels().subscribe(levelList => {
+      this.levels = levelList;
+    });
+    this.testService.getQuizzes().subscribe(quizList => {
+      this.quizzes = quizList;
+    });
+    this.testService.getCategories().subscribe(categoryList => {
+      this.categories = categoryList;
     });
   }
 }
--- src/app/tests/model/test.ts
+++ src/app/tests/model/test.ts
@@ -1,9 +1,12 @@
 import {Level} from "./level";
+import {Category} from "./category";
 
 export interface Test {
     id?: number,
     name?: string,
     level?: Level,
+    quiz?: any,
+    category?: Category[],
     passScore?: number,
     maxTime?: string
 }
