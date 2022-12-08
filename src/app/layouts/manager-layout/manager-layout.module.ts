f8a73049270dc249384e10abb5ea872002ab9ec8
TuLiP-411 <65971089+TuLiP-411@users.noreply.github.com>
2022-12-08 22:30:46 +0700

    merge coflict

--- src/app/layouts/manager-layout/manager-layout.module.ts
+++ src/app/layouts/manager-layout/manager-layout.module.ts
@@ -1,52 +1,57 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import { LbdModule } from '../../lbd/lbd.module';
-import { NguiMapModule} from '@ngui/map';
+import {LbdModule} from '../../lbd/lbd.module';
+import {NguiMapModule} from '@ngui/map';
 
-import { ManagerLayoutRoutes } from './manager-layout.routing';
+import {ManagerLayoutRoutes} from './manager-layout.routing';
 
-import { HomeComponent } from '../../home/home.component';
-import { UserComponent } from '../../user/user.component';
-import {QuizComponent} from "../../quiz/quiz.component";
-import { TablesComponent } from '../../tables/tables.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
+import {HomeComponent} from '../../home/home.component';
+import {UserComponent} from '../../user/user.component';
+import {TablesComponent} from '../../tables/tables.component';
+import {TypographyComponent} from '../../typography/typography.component';
+import {IconsComponent} from '../../icons/icons.component';
+import {MapsComponent} from '../../maps/maps.component';
+import {NotificationsComponent} from '../../notifications/notifications.component';
 import {ListTestComponent} from "../../tests/crud/list-test/list-test.component";
 import {CreateTestComponent} from "../../tests/crud/create-test/create-test.component";
 import {ListComponent} from "../../category/list/list.component";
 import {CreateComponent} from "../../category/create/create.component";
 import {MatCheckboxModule} from "@angular/material/checkbox";
+import {ListQuizComponent} from "../../quiz/crud/list-quiz/list-quiz.component";
+import {UpdateQuizComponent} from "../../quiz/crud/update-quiz/update-quiz.component";
+import {CreateQuizComponent} from "../../quiz/crud/create-quiz/create-quiz.component";
 
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forChild(ManagerLayoutRoutes),
-    FormsModule,
-    LbdModule,
-    NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=YOUR_KEY_HERE'}),
-    ReactiveFormsModule,
-    MatCheckboxModule
-  ],
-  declarations: [
-    HomeComponent,
-    UserComponent,
-    QuizComponent,
-      ListComponent,
-      CreateComponent,
-    ListTestComponent,
-    CreateTestComponent,
-    TablesComponent,
-    TypographyComponent,
-    IconsComponent,
-    MapsComponent,
-    NotificationsComponent
-  ]
+    imports: [
+        CommonModule,
+        RouterModule.forChild(ManagerLayoutRoutes),
+        FormsModule,
+        LbdModule,
+        NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=YOUR_KEY_HERE'}),
+        ReactiveFormsModule,
+        MatCheckboxModule
+    ],
+    declarations: [
+        HomeComponent,
+        UserComponent,
+        ListQuizComponent,
+        UpdateQuizComponent,
+        CreateQuizComponent,
+        ListComponent,
+        CreateComponent,
+        ListTestComponent,
+        CreateTestComponent,
+        TablesComponent,
+        TypographyComponent,
+        IconsComponent,
+        MapsComponent,
+        NotificationsComponent
+    ]
 })
 
-export class ManagerLayoutModule {}
+export class ManagerLayoutModule {
+}
