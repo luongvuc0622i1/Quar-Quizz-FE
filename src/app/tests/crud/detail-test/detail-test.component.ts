66694db022ecb39927390695ca08d49e45aa7553
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2022-12-09 13:12:41 +0700

    done detail test

--- src/app/tests/crud/detail-test/detail-test.component.ts
+++ src/app/tests/crud/detail-test/detail-test.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Test} from "../../model/test";
+import {TestService} from "../../service/test.service";
+import {ActivatedRoute, ParamMap} from "@angular/router";
+import {Observable} from "rxjs";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-detail-test',
@@ -6,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail-test.component.scss']
 })
 export class DetailTestComponent implements OnInit {
+  test: Test;
+  id: number;
+  panelOpenState = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private testService: TestService,
+              private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      this.id = +paramMap.get('id');
+    });
   }
 
+  ngOnInit(): void {
+    this.testService.findById(this.id).subscribe(test => {
+      this.test = test;
+    });
+  }
 }
