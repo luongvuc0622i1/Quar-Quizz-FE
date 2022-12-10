0570a796eaf0d863e19b98b9a67630014c90506d
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2022-12-10 17:18:37 +0700

    form edit password

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -12,4 +12,29 @@ export class UserComponent implements OnInit {
   ngOnInit() {
   }
 
+  myFunction1() {
+    var x = document.getElementById("inputCur");
+    this.change(x);
+  }
+
+  myFunction2() {
+    var x = document.getElementById("inputNew");
+    this.change(x);
+  }
+
+  myFunction3() {
+    var x = document.getElementById("inputRe");
+    this.change(x);
+  }
+
+  change(x) {
+    // @ts-ignore
+    if (x.type === "password") {
+      // @ts-ignore
+      x.type = "text";
+    } else {
+      // @ts-ignore
+      x.type = "password";
+    }
+  }
 }
