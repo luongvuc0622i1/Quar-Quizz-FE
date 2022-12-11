815bf1bd47f9141ebfe89077007cfc4cd3834079
TuLiP-411 <65971089+TuLiP-411@users.noreply.github.com>
2022-12-12 00:58:38 +0700

    done delete and doung update

--- src/app/quiz/crud/list-quiz/list-quiz.component.ts
+++ src/app/quiz/crud/list-quiz/list-quiz.component.ts
@@ -1,52 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Quiz} from "../../model/quiz";
 import {QuizService} from "../../service/quiz.service";
 import {resourceChangeTicket} from "@angular/compiler-cli/src/ngtsc/core";
+import Swal from "sweetalert2";
+import {ActivatedRoute, Router} from "@angular/router";
 
 
 @Component({
-  selector: 'app-list-quiz',
-  templateUrl: './list-quiz.component.html',
-  styleUrls: ['./list-quiz.component.scss']
+    selector: 'app-list-quiz',
+    templateUrl: './list-quiz.component.html',
+    styleUrls: ['./list-quiz.component.scss']
 })
 export class ListQuizComponent implements OnInit {
-  quizzes: Quiz[] =[];
+    quizzes: Quiz[] = [];
 
-  constructor(private quizService: QuizService) {
-  }
-
-  ngOnInit(): void {
-    this.getAll();
-  }
-  ngAfterViewInit(){
-    $(document).ready(function(){
-      $("#myInput").on("keyup", function() {
-        // @ts-ignore
-        var value = $(this).val().toLowerCase();
-        // @ts-ignore
-        $("#myTable tr").filter(function() {
-          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-        });
-      });
-    });
-  };
-
-
-  getAll() {
-    this.quizService.getAll().subscribe(quizList => {
-    this.quizzes = quizList;
-    });
-  }
-
-  selectedId : any = '';
-
-  private getChild(quiz: Quiz, id: number) {
-    // Check if already expanded
-    if(this.selectedId == id){
-      this.selectedId = '';
-    }else{
-      this.selectedId = id;
+    constructor(private quizService: QuizService, private activatedRoute: ActivatedRoute,
+                private router: Router) {
     }
-  }
+
+    ngOnInit(): void {
+        this.getAll();
+    }
+
+    ngAfterViewInit() {
+        $(document).ready(function () {
+            $("#myInput").on("keyup", function () {
+                // @ts-ignore
+                var value = $(this).val().toLowerCase();
+                // @ts-ignore
+                $("#myTable tr").filter(function () {
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                });
+            });
+        });
+    };
+
+
+    getAll() {
+        this.quizService.getAll().subscribe(quizList => {
+            this.quizzes = quizList;
+        });
+    }
+
+    selectedId: any = '';
+
+    private getChild(quiz: Quiz, id: number) {
+        // Check if already expanded
+        if (this.selectedId == id) {
+            this.selectedId = '';
+        } else {
+            this.selectedId = id;
+        }
+    }
+
+    deleteQuiz(id) {
+        Swal.fire({
+            title: 'Delete quiz',
+            text: "Are you sure to delete this quiz?",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Delete'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                this.quizService.delete(id).subscribe(() => {
+                    this.router.navigate(['/manager/quiz/list']).then(() => {
+                        location.reload()
+                    })
+                }, e => {
+                    console.log(e);
+                });
+            }
+        })
+
+
+    }
 
 }
--- src/app/quiz/crud/update-quiz/update-quiz.component.ts
+++ src/app/quiz/crud/update-quiz/update-quiz.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Level} from "../../model/level";
+import {TypeQuizzes} from "../../model/typequizzes";
+import {Category} from "../../model/category";
+import {QuizService} from "../../service/quiz.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+declare var $: any;
 
 @Component({
   selector: 'app-update-quiz',
@@ -6,10 +12,146 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-quiz.component.scss']
 })
 export class UpdateQuizComponent implements OnInit {
+  levels: Level[] = [];
+  typeQuizzes: TypeQuizzes[] = [];
+  categories: Category[] = [];
+  categoriesChoice: Category[] = [];
 
-  constructor() { }
+  option1: any;
+  option2: any;
+  option3: any;
+  option4: any;
+
+  constructor(private quizService: QuizService) {
+  }
 
   ngOnInit(): void {
+    this.getAll();
+  }
+
+  quizForm: FormGroup = new FormGroup({
+    name: new FormControl("", [Validators.required]),
+    answer1: new FormControl("", [Validators.required]),
+    answer2: new FormControl("", [Validators.required]),
+    answer3: new FormControl("", [Validators.required]),
+    answer4: new FormControl("", [Validators.required]),
+    correct_answer: new FormControl("", [Validators.required]),
+    level: new FormControl("", [Validators.required]),
+    typeQuiz: new FormControl("", [Validators.required]),
+    category: new FormControl("")
+  })
+
+  get name() {
+    return this.quizForm.get('name')
+  }
+
+  get answer1() {
+    return this.quizForm.get('answer1')
+  }
+
+  get answer2() {
+    return this.quizForm.get('answer2')
+  }
+
+  get answer3() {
+    return this.quizForm.get('answer3')
+  }
+
+  get answer4() {
+    return this.quizForm.get('answer4')
+  }
+
+
+  get correctAnswer() {
+    return this.quizForm.get('correct_answer')
+  }
+
+  get level() {
+    return this.quizForm.get('level')
+  }
+
+  get typeQuiz() {
+    return this.quizForm.get('typeQuiz')
+  }
+
+
+  submit() {
+    const quiz = this.quizForm.value;
+    console.log(quiz);
+    const quiz1 = this.change(quiz);
+    console.log(quiz1);
+    this.quizService.save(quiz1).subscribe(() => {
+      this.quizForm.reset();
+      this.showNotification('top', 'center')
+    }, error => {
+      console.log(error)
+    });
+  }
+
+  getAll() {
+    this.quizService.getLevels().subscribe(levelList => {
+      this.levels = levelList;
+    });
+    this.quizService.getTypeQuizzes().subscribe(typeQuizzesList => {
+      this.typeQuizzes = typeQuizzesList;
+    });
+    this.quizService.getCategories().subscribe(categoryList => {
+      this.categories = categoryList;
+    });
+  }
+
+  change(quiz: any) {
+    let id;
+    let arLevel;
+    let arCategory = [];
+    let quiz1;
+    let correct_answer1: String = "";
+    let arTypeQuizzes;
+    let answer = quiz.answer1 + ";" + quiz.answer2 + ";" + quiz.answer3 + ";" + quiz.answer4;
+
+    for (let i = 0; i < this.categoriesChoice.length; i++) {
+      id = this.categoriesChoice[i].id;
+      arCategory.push({id});
+    }
+    correct_answer1 += quiz.correct_answer.join(";");
+
+    id = quiz.level;
+    arLevel = {id};
+
+    id = quiz.typeQuiz;
+    arTypeQuizzes = {id};
+
+    quiz1 = {
+      answer: answer,
+      correct_answer: correct_answer1,
+      name: quiz.name,
+      typeQuizzes: arTypeQuizzes,
+      level: arLevel,
+      categories: arCategory
+    }
+    return quiz1;
+  }
+
+  choiceCategory(value: any) {
+    console.log(value);
+    this.categoriesChoice.push(value);
+  }
+
+  showNotification(from, align) {
+    // const type = ['','info','success','warning','danger'];
+    //
+    // var color = Math.floor((Math.random() * 4) + 1);
+    $.notify({
+      icon: "pe-7s-check",
+      message: "Create new quiz successfully!"
+    }, {
+      type: 'success',
+      timer: 1000,
+      placement: {
+        from: from,
+        align: align
+      }
+    });
   }
 
 }
